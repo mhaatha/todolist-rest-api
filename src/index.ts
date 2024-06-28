c39775dc9da8f95414c87da73f69a0427693d885
Hafidz Athaya <mhafidza528@gmail.com>
2024-06-28 20:04:56 +0800

    update middleware

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import 'dotenv/config';
-import express, { Request, Response } from 'express';
-import authApi from './routes/auth.routes';
-import userApi from './routes/user.routes';
-import todolistApi from './routes/todolist.routes';
+import express from 'express';
+import router from './routes';
 
 const app = express();                                
 const port = process.env.PORT || 3000;
 
-app.use(authApi);
-app.use(userApi);
-app.use(todolistApi);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Server is online');
+});   
+
+app.use('/api/v1', router);      
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
