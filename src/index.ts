deed85f3e1068f210102edbcd871a447d299c6d3
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-01 17:59:43 +0800

    add error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
 import router from './routes';
+import { errorMiddleware } from './middlewares/error.middleware';
 
 const app = express();                                
 const port = process.env.PORT || 3000;
 
+// Middleware Parsing JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,7 +14,10 @@ app.get('/', (req, res) => {
   res.send('Server is online');
 });   
 
-app.use('/api/v1', router);      
+app.use('/api/v1', router);   
+
+// Middleware Error Handling
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
