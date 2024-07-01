3d9792e7be9ebb36aaae2c010950803e490280dc
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-01 13:27:23 +0800

    update auth controllers

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,11 +1,9 @@
 import { Request, Response } from 'express';
 
-const authRegister = async (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response) => {
   res.send('REGISTER');
 }
 
-const authLogin = async (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response) => {
   res.send('LOGIN');
 }
-
-export { authRegister, authLogin };
