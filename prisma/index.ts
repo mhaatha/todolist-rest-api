b3d93b8f52e08c102fb01f1e37acaef26c59963a
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-02 13:19:26 +0800

    prisma logging

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,4 +1,39 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { logger } from "../src/configs/logger";
 
-export { prisma };
+export const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query'
+    },
+    {
+      emit: 'event',
+      level: 'error'
+    },
+    {
+      emit: 'event',
+      level: 'info'
+    },
+    {
+      emit: 'event',
+      level: 'warn'
+    }
+  ]
+});
+
+prisma.$on('error', (e) => {
+  logger.error(e);
+});
+
+prisma.$on('warn', (e) => {
+  logger.warn(e);
+});
+
+prisma.$on('info', (e) => {
+  logger.info(e);
+});
+
+prisma.$on('query', (e) => {
+  logger.info(e);
+});
