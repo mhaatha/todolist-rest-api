56084dd9cef35338cd0ccd1f7b9c9c0bcd1208a9
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-02 13:35:37 +0800

    logging

---
+++ src/configs/logger.ts
@@ -0,0 +1,29 @@
+import winston from 'winston';
+import config from './config';
+
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+const customFormat = winston.format.printf(({ level, message, meta }) => {
+  let metaString = meta ? `\n${JSON.stringify(meta, null, 2)}` : '' ;
+  return `${level}: ${message} ${metaString}`
+});
+
+export const logger = winston.createLogger({
+  level: config.env === 'development' ? 'debug' : 'info',
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    config.env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
+    winston.format.splat(),
+    customFormat
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ['error'],
+    }),
+  ],
+});
