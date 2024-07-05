7ded6a34796407d2d41cfcc6944db81aa0b3abfe
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-05 14:51:38 +0800

    update variable name

--- src/validations/auth-validation.ts
+++ src/validations/auth-validation.ts
@@ -1,6 +1,6 @@
 import { z, ZodType } from "zod";
 
-export const register: ZodType = z.object({
+export const registerAndLogin: ZodType = z.object({
   username: z.string().min(6).max(36),
   password: z.string().min(6)
 });
