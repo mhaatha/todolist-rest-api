59a205a4c6807aad92b6f404529b2888784cd428
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-05 13:18:05 +0800

    logger

--- src/configs/logger.ts
+++ src/configs/logger.ts
@@ -1,30 +1,16 @@
-import winston from 'winston';
-import config from './config';
-
-const enumerateErrorFormat = winston.format((info) => {
-  if (info instanceof Error) {
-    Object.assign(info, { message: info.stack });
-  }
-  return info;
-});
-
-// Meta is prisma property for error
-const customFormat = winston.format.printf(({ level, message, meta }) => {
-  let metaString = meta ? `\n${JSON.stringify(meta, null, 2)}` : '' ;
-  return `${level}: ${message} ${metaString}`
-});
+import winston from "winston";
+import config from "./config";
 
 export const logger = winston.createLogger({
   level: config.env === 'development' ? 'debug' : 'info',
   format: winston.format.combine(
-    enumerateErrorFormat(),
     config.env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
     winston.format.splat(),
-    customFormat
+    winston.format.printf(({ level, message }) => `${level}: ${message}`)
   ),
   transports: [
     new winston.transports.Console({
       stderrLevels: ['error'],
-    }),
-  ],
+    })
+  ]
 });
