b3e75231dd8ffacc2feef4c24787649e3e53fe0b
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-05 13:16:40 +0800

    update import name

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
-import authRoute from './auth.routes';
-import userRoute from './user.routes';
-import todolistRoute from './todolist.routes';
+import authRoute from './auth-routes';
+import userRoute from './user-routes';
+import todolistRoute from './todolist-routes';
 
 const router = express.Router();
 
