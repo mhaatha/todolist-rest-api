4c98ce7e858147e8df09a8ea206ff84865a73f30
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-05 14:52:04 +0800

    generate token service

---
+++ src/services/token-service.ts
@@ -0,0 +1,34 @@
+import { tokenTypes } from "../configs/tokens";
+import * as jwt from "jsonwebtoken";
+import moment from 'moment';
+import config from "../configs/config";
+import { User } from "@prisma/client";
+
+export const generateToken = async (userId: string, expires: moment.Moment, type: string, secret = config.jwt.secret!) => {
+  const payload = {
+    sub: userId,
+    iat: moment().unix(),
+    exp: expires.unix(),
+    type
+  };
+  return jwt.sign(payload, secret);
+}
+
+export const generateAuthToken = async (user: User) => {
+  const accessTokenExpires = moment().add(config.jwt.accessExpirationMinutes, 'minutes');
+  const accessToken = await generateToken(user.id, accessTokenExpires, tokenTypes.ACCESS);
+
+  const refreshTokenExpires = moment().add(config.jwt.refreshExpirationDays, 'days');
+  const refreshToken = await generateToken(user.id, refreshTokenExpires, tokenTypes.REFRESH);
+
+  return {
+    access: {
+      token: accessToken,
+      expires: accessTokenExpires.toDate()
+    },
+    refresh: {
+      token: refreshToken,
+      expires: refreshTokenExpires.toDate()
+    }
+  }
+}
