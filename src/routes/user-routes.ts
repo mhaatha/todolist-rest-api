063f3cda906e7406f15dfb6e9e51bd9bb428fdc3
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-06 16:58:45 +0800

    user routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,15 +1,11 @@
-import express, { Request, Response } from 'express';
+import * as userController from '../controllers/user-controller';
+import express from 'express';
+
 const userRoute = express.Router();
 
 userRoute.route('/:userId')
-  .get((req: Request, res: Response) => {
-    res.send('GET USER BY ID');
-  })
-  .put((req: Request, res: Response) => {
-    res.send('UPDATE USER BY ID');
-  })
-  .delete((req: Request, res: Response) => {
-    res.send('DELETE USER BY ID');
-  });
+  .get(userController.getId)
+  .put(userController.update)
+  .delete(userController.deleted);
 
 export default userRoute;
