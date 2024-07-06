69816c6be582a4eef3c763da889e762b15216e96
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-06 16:39:47 +0800

    simplify the function get user by username

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -7,17 +7,14 @@ import * as authValidation from '../validations/auth-validation';
 import { ResponseError } from '../utils/response-error';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 import { User } from '@prisma/client';
+import { getUserByUsername } from '../services/user-service';
 
 export const register = async (data: userModel.RegisterAndLoginRequest): Promise<userModel.RegisterResponse> => {
   const registerRequest: userModel.RegisterAndLoginRequest = validate(authValidation.registerAndLogin, data);
 
   // VALIDATION: Cannot have the same username
-  const totalUserWithSameUsername: number = await prisma.user.count({
-    where: {
-      username: registerRequest.username
-    }
-  });
-  if (totalUserWithSameUsername !== 0) {
+  const totalUserWithSameUsername: User | null = await getUserByUsername(registerRequest.username);
+  if (totalUserWithSameUsername) {
     throw new ResponseError(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST, 'Username already taken');
   }
 
@@ -35,20 +32,14 @@ export const register = async (data: userModel.RegisterAndLoginRequest): Promise
 }
 
 export const login = async (data: userModel.RegisterAndLoginRequest): Promise<User> => {
-  const loginRequest: userModel.RegisterAndLoginRequest = validate(authValidation.registerAndLogin, data);
-
   // VALIDATION: Is username exists in the database
-  const user = await prisma.user.findUnique({
-    where: {
-      username: loginRequest.username
-    }
-  }); 
+  const user: User | null = await getUserByUsername(data.username);
   if (!user) {
     throw new ResponseError(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED, 'Username or password is incorrect');
   }
   
   // VALIDATION: Is password correct
-  const isMatch = await bcrypt.compare(loginRequest.password, user.password);
+  const isMatch = await bcrypt.compare(data.password, user.password);
   if (!isMatch) {
     throw new ResponseError(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED, 'Username or password is incorrect');
   }
