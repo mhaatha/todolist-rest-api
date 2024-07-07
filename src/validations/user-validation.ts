eebca3a72cd7db2f43fc6868fc1e3a89891dd7f2
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-07 16:14:40 +0800

    validation for update user

---
+++ src/validations/user-validation.ts
@@ -0,0 +1,10 @@
+import { z, ZodType } from 'zod';
+
+export const registerAndLogin: ZodType = z.object({
+  username: z.string().min(6).max(36),
+  password: z.string().min(6)
+});
+
+export const updateUserBody: ZodType = z.object({
+  username: z.string().min(6).max(36)
+}); 
