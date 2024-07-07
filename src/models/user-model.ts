be2a07918a23202324e3902b76dabb3ec48da9a8
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-07 16:22:34 +0800

    user model

---
+++ src/models/user-model.ts
@@ -0,0 +1,26 @@
+import { User } from "@prisma/client";
+
+export interface UsernamePasswordRequest {
+  username: string;
+  password: string;
+}
+
+export interface IdUsernameResponse {
+  id: string;
+  username: string;
+}
+
+export function toAuthResponse(user: User): IdUsernameResponse {
+  return {
+    id: user.id,
+    username: user.username
+  }
+}
+
+export interface UsernameSchema {
+  username: string;
+}
+
+export interface UserIdSchema {
+  userId: string;
+}
