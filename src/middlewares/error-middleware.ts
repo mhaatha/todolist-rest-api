3e135bc66e8a691f9c13f6f16b8bfe5d7de0f13e
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-07 16:28:59 +0800

    error handling for jwt error

--- src/middlewares/error-middleware.ts
+++ src/middlewares/error-middleware.ts
@@ -1,7 +1,8 @@
-import { Request, Response, NextFunction } from 'express';
 import { ZodError } from 'zod';
-import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 import { ResponseError } from '../utils/response-error';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+import { Request, Response, NextFunction } from 'express';
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 
 export const errorHandler = (err: ResponseError, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof ZodError) {
@@ -14,6 +15,18 @@ export const errorHandler = (err: ResponseError, req: Request, res: Response, ne
       status: StatusCodes.BAD_REQUEST,
       error: ReasonPhrases.BAD_REQUEST,
       message: errors
+    });
+  } else if (err instanceof JsonWebTokenError) {
+    res.status(StatusCodes.UNAUTHORIZED).json({
+      status: StatusCodes.UNAUTHORIZED,
+      error: ReasonPhrases.UNAUTHORIZED,
+      message: 'Invalid token'
+    });
+  } else if (err instanceof TokenExpiredError) {
+    res.status(StatusCodes.UNAUTHORIZED).json({
+      status: StatusCodes.UNAUTHORIZED,
+      error: ReasonPhrases.UNAUTHORIZED,
+      message: 'Token expired'
     });
   } else if (err instanceof ResponseError) {
     res.status(StatusCodes.BAD_REQUEST).json({
