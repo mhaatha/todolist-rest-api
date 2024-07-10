4539c195ba0da23f98e98290fc900f2a3ec5a51d
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-10 21:07:19 +0800

    todolist prototype controller

---
+++ src/controllers/todolist-controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const create = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const getAll = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const update = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const deleted = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+}
