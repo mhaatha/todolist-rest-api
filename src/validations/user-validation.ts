db0ba4ecb7ee76b82ea3fdacebd524081047606b
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-10 17:30:23 +0800

    add regex to username

--- src/validations/user-validation.ts
+++ src/validations/user-validation.ts
@@ -1,10 +1,10 @@
 import { z, ZodType } from 'zod';
 
 export const registerAndLogin: ZodType = z.object({
-  username: z.string().min(6).max(36),
+  username: z.string().min(6).max(36).regex(/^[a-z]*$/, { message: 'Username cannot contain spaces or capital letters' }),
   password: z.string().min(6)
 }).strict();
 
 export const updateUserBody: ZodType = z.object({
-  username: z.string().min(6).max(36)
+  username: z.string().min(6).max(36).regex(/^[a-z]*$/, { message: 'Username cannot contain spaces or capital letters' })
 }).strict(); 
