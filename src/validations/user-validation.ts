1a5bf5166070c62ec09f1df6c9b04701352df55e
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-10 17:07:31 +0800

    strict

--- src/validations/user-validation.ts
+++ src/validations/user-validation.ts
@@ -3,8 +3,8 @@ import { z, ZodType } from 'zod';
 export const registerAndLogin: ZodType = z.object({
   username: z.string().min(6).max(36),
   password: z.string().min(6)
-});
+}).strict();
 
 export const updateUserBody: ZodType = z.object({
   username: z.string().min(6).max(36)
-}); 
+}).strict(); 
