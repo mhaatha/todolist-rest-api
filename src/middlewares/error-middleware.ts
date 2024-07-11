6fb4473fa71ce79447c5585652560aaf9d8fdacb
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-11 15:41:58 +0800

    add prisma error handling

--- src/middlewares/error-middleware.ts
+++ src/middlewares/error-middleware.ts
@@ -1,8 +1,26 @@
 import { ZodError } from 'zod';
 import { ResponseError } from '../utils/response-error';
 import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { Request, Response, NextFunction } from 'express';
 import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
+
+const handlePrismaError = (err: PrismaClientKnownRequestError) => {
+  switch (err.code) {
+    case 'P2002':
+      // Handling duplicate key errors
+      return new ResponseError(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST, 'Duplicate Key');
+    case 'P2014':
+      // Handling invalid id errors
+      return new ResponseError(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST, 'Invalid Id');
+    case 'P2003':
+      // Handling invalid data errors
+      return new ResponseError(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST, `Invalid Data ${err}`);
+    default:
+      // Handling all other errors
+      return new ResponseError(StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR, 'Internal Server Error');
+  }
+};
 
 export const errorHandler = (err: ResponseError, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof ZodError) {
@@ -15,6 +33,14 @@ export const errorHandler = (err: ResponseError, req: Request, res: Response, ne
       status: StatusCodes.BAD_REQUEST,
       error: ReasonPhrases.BAD_REQUEST,
       message: errors
+    });
+  } else if (err instanceof PrismaClientKnownRequestError) {
+    const errors = handlePrismaError(err);
+    
+    res.status(errors.status).json({
+      status: errors.status,
+      error: errors.error,
+      message: errors.message
     });
   } else if (err instanceof JsonWebTokenError) {
     res.status(StatusCodes.UNAUTHORIZED).json({
