9ae527df6d288227906f91ffdfb8219d59a3e6c9
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-12 15:03:24 +0800

    add tags route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import authRoute from './auth-routes';
 import userRoute from './user-routes';
 import todolistRoute from './todolist-routes';
+import tagRoute from './tag-routes';
 
 const router = express.Router();
 
@@ -17,6 +18,10 @@ const defaultRoute: { path: string, route: Router }[] = [
   {
     path: '/todolists',
     route: todolistRoute
+  },
+  {
+    path: '/tags',
+    route: tagRoute
   }
 ];  
       
