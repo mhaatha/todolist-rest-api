be8f7e0dfa39836d08281e5332a3da551e297534
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-13 17:11:13 +0800

    install optional middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,39 @@
-import express from 'express';
+import cors from 'cors';
 import router from './routes';
+import helmet from 'helmet';
+import express from 'express';
+import compression from 'compression';
 import { errorHandler } from './middlewares/error-middleware';
+import { ResponseError } from './utils/response-error';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+const xssClean = require('xss-clean') as () => express.RequestHandler;
 
 export const app = express();
+
+// HTTP Header security
+app.use(helmet());
 
 // Middleware Parsing JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Sanitize request data
+app.use(xssClean());
+
+// Gzip compression
+app.use(compression());
+
+// CORS
+app.use(cors());
+app.options('*', cors());
+
+// Main API
 app.use('/api/v1', router);
+
+// Send 404 if route is not exists
+app.use((req, res, next) => {
+  next(new ResponseError(StatusCodes.NOT_FOUND, ReasonPhrases.NOT_FOUND, 'Endpoint is not found'));
+});
 
 // Middleware Error Handler
 app.use(errorHandler);
--- src/types/xss-clean.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'xss-clean' {
-  import { RequestHandler } from 'express';
-  const xss: () => RequestHandler;
-  export default xss;
-}
