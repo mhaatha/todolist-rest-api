c7ba851889a9743e68e42f5f81f5f1f44df241f8
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-15 21:45:26 +0800

    update key name

--- src/validations/todolist-tag-validation.ts
+++ src/validations/todolist-tag-validation.ts
@@ -3,6 +3,6 @@ import { z, ZodType } from 'zod';
 const uuidRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
 
 export const todolistTagBodyRequest: ZodType = z.object({
-  todolistId: z.string().regex(uuidRegex, { message: 'Invalid uuid' }),
-  tagId: z.string().regex(uuidRegex, { message: 'Invalid uuid' })
+  todolist_id: z.string().regex(uuidRegex, { message: 'Invalid uuid' }),
+  tag_id: z.string().regex(uuidRegex, { message: 'Invalid uuid' })
 }).strict();
